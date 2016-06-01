1604bf01fee002bbb770f9d44240018a4834926f
Didier Baczynski <didier.baczynski@gmail.com>
2016-05-31 21:56:24 -0400

    First version of LongDistanceApps

--- src/main/java/com/windsurf/LongDistanceAppsApplication.java
+++ src/main/java/com/windsurf/LongDistanceAppsApplication.java
@@ -3,19 +3,27 @@ package com.windsurf;
 import com.windsurf.config.AppConfig;
 import com.windsurf.entities.LongDistanceRace;
 import com.windsurf.entities.OBXRace;
-import org.springframework.boot.SpringApplication;
+import com.windsurf.entities.Racer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class LongDistanceAppsApplication {
 
 	public static void main(String[] args) {
+		List <Racer> racers;
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 		LongDistanceRace race_1 = context.getBean("race", OBXRace.class);
 
-		System.out.println(race_1.runRace());
+		racers = race_1.runRace();
+		for (Racer racer:racers) {
+			System.out.println(racer.getName());
+			System.out.println(racer.getAge());
+			System.out.println(racer.getId());
+		}
 	}
 }
